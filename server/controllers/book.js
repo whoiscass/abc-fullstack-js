b9896fe727e6ebec606eb48265a0fa3db158a09e
whoiscass <hctr.kz@gmail.com>
2019-11-14 00:55:09 -0500

    book controller

---
+++ server/controllers/book.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const queries = require('../utils/queries')
+const Book = require('../models/book')
+
+module.exports = {
+  async createBook(req, res) {
+    try {
+      const { body } = req
+      const response = await queries.saveObject(Book, body)
+      res.status(200).json(response)
+    } catch(err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+  },
+
+  async getBooks(req, res) {
+    try {
+      const response = await queries.getObjectsByParams(Book, {}, [])
+      res.status(200).json(response.reverse())
+    } catch(err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+  },
+
+  async getBooksById(req, res) {
+    try {
+      const { params: { id } } = req.params
+      const response = await queries.getOneObjectByParams(Book, { '_id': id }, [])
+      res.status(200).json(response)
+    } catch(err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+  },
+
+  async updateBook(req, res) {
+    try {
+      const { body, params: { id } } = req
+      const response = await queries.updateObjectsByParams(Book, { '_id': id }, body)
+      res.status(200).json(response)
+    } catch(err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+  },
+
+  async deleteBook(req, res) {
+    try {
+      const { params: { id } } = req.params
+      const response = await queries.deleteObjectsByParams(Book, { '_id': id })
+      res.status(200).json(response)
+    } catch(err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+  }
+}
